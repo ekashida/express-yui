24bc477c21c6171940a4cbcfe0fd140f1bf7ba89
Eugene Kashida <ekashida@gmail.com>
2014-01-24 09:21:10 -0800

    minor refactor with comments

--- examples/perf-hooks/app.js
+++ examples/perf-hooks/app.js
@@ -22,89 +22,143 @@ app.yui.patchClient(function (Y) {
         perf     = YUI.Env.perf = {},
         loader   = Y.Env._loader,
         sequence = 0,
+        loaderMethods,
         current,
-        useId,
-        orig;
+        orig,
+        len,
+        i;
 
     function getTime () {
         return new Date().getTime();
     }
 
-    function addTimings (name, context, args) {
-        var now = getTime(),
-            key = name + '__' + sequence++,
-            timings = current[key] = {};
+    // Generic way to instrument a function. Uses sequencing to support
+    // multiple invocations during a single `Y.use()` call.
+    function instrument (context, name) {
+        var orig = context[name];
 
-        timings.start = now;
-        timings.since_use_start = now - current._use.start;
+        context[name] = function () {
+            var now     = getTime(),
+                key     = name + '__' + sequence++,
+                timing  = current[key] = {};
 
-        orig[name].apply(context, args);
+            timing.since_use_start = now - current.start;
+            timing.start = now;
 
-        timings.end = getTime();
-        timings.duration = timings.end - timings.start;
+            orig.apply(context, arguments);
+
+            timing.end = getTime();
+            timing.duration = timing.end - timing.start;
+        };
     }
 
     orig = {
-        get_js:     Y.Get.js,
-        _use:       Y._use,
-
-        _sort:      loader._sort,
-        _reduce:    loader._reduce,
-        _insert:    loader._insert,
-        _explode:   loader._explode,
-        calculate:  loader.calculate
+        getjs:  Y.Get.js,
+        _use:   Y._use
     };
 
+    // Start of the `Y.use()` invocation
     Y._use = function () {
-        var modules     = arguments[0],
-            now         = getTime();
+        var now = getTime(),
+            useId;
 
-        // unique id for current `use` operation
-        useId = modules.join(',') + '__' + now;
+        // unique id for current `use` invocation
+        useId = arguments[0].join(',') + '__' + now;
 
-        current = perf[useId] = {
-            _use: {
-                start: now
-            }
-        };
-
+        current = perf[useId] = { start: now };
         orig._use.apply(Y, arguments);
     };
 
+    // Effective end of the `Y.use()` invocation before things get async
     Y.Get.js = function () {
-        var now = getTime();
-        current._use.end = now;
-        current._use.duration = current._use.end - current._use.start;
-        orig.get_js.apply(Y.Get, arguments);
+        current.end = getTime();
+        current.duration = current.end - current.start;
+        orig.getjs.apply(Y.Get, arguments);
     };
 
-    loader._sort = function () {
-        addTimings('_sort', loader, arguments);
-    };
+    loaderMethods = [
+        '_sort',
+        '_reduce',
+        '_insert',
+        '_explode',
+        'calculate'
+    ];
 
-    loader._reduce = function () {
-        addTimings('_reduce', loader, arguments);
-    };
+    for (i = 0, len = loaderMethods.length; i < len; i += 1) {
+        instrument(loader, loaderMethods[i]);
+    }
 
-    loader._insert = function () {
-        addTimings('_insert', loader, arguments);
-    };
-
-    loader._explode = function () {
-        addTimings('_explode', loader, arguments);
-    };
-
-    loader.calculate = function () {
-        addTimings('calculate', loader, arguments);
-    };
 });
+
+// Example output:
+// {
+//     "stencil-toggle,stencil,stencil-carousel,stencil-bquery,stencil-sticker,stencil-scrollview,af-applets,app-base,af-rapid__1390583569230": {
+//         "start": 1390583569230,
+//         "calculate__0": {
+//             "since_use_start": 0,
+//             "start": 1390583569230,
+//             "end": 1390583569250,
+//             "duration": 20
+//         },
+//         "_explode__1": {
+//             "since_use_start": 3,
+//             "start": 1390583569233,
+//             "end": 1390583569244,
+//             "duration": 11
+//         },
+//         "_reduce__2": {
+//             "since_use_start": 14,
+//             "start": 1390583569244,
+//             "end": 1390583569244,
+//             "duration": 0
+//         },
+//         "_sort__3": {
+//             "since_use_start": 14,
+//             "start": 1390583569244,
+//             "end": 1390583569250,
+//             "duration": 6
+//         },
+//         "_insert__4": {
+//             "since_use_start": 21,
+//             "start": 1390583569251,
+//             "end": 1390583569267,
+//             "duration": 16
+//         },
+//         "calculate__5": {
+//             "since_use_start": 22,
+//             "start": 1390583569252,
+//             "end": 1390583569260,
+//             "duration": 8
+//         },
+//         "_explode__6": {
+//             "since_use_start": 22,
+//             "start": 1390583569252,
+//             "end": 1390583569254,
+//             "duration": 2
+//         },
+//         "_reduce__7": {
+//             "since_use_start": 24,
+//             "start": 1390583569254,
+//             "end": 1390583569254,
+//             "duration": 0
+//         },
+//         "_sort__8": {
+//             "since_use_start": 24,
+//             "start": 1390583569254,
+//             "end": 1390583569260,
+//             "duration": 6
+//         },
+//         "end": 1390583569262,
+//         "duration": 32
+//     }
+// }
 
 // template engine
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 // creating a page with YUI embeded
-app.get('/', expyui.expose(), function (req, res, next) {
+app.get('/', expyui.expose(), function (req, res) {
     res.render('page');
 });
 
